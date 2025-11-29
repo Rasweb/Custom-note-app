7324c668daf9deed8d54a83530c4fc4831ba2e77
Rasweb <rasmuspalm@fastmail.se>
2025-11-29 17:02:19 +0100

    Replace react-markdown view with mdx

--- src/components/Note/NoteContent.tsx
+++ src/components/Note/NoteContent.tsx
@@ -2,9 +2,12 @@ import * as Types from "@/types/types"
 import { ViewMarkdown } from "../markdown/ViewMarkdown"
 
 export function NoteContent({note}: {note: Types.NoteType}) { 
+    
     return (
         <>
+            <div className='prose prose-invert'>
             <ViewMarkdown note={note}></ViewMarkdown>
+            </div>
         </>
     )
 }
--- src/components/markdown/ViewMarkdown.tsx
+++ src/components/markdown/ViewMarkdown.tsx
@@ -1,20 +1,45 @@
-import ReactMarkdown  from 'react-markdown'
 import * as Types from "@/types/types"
+import {BlockTypeSelect, BoldItalicUnderlineToggles, CodeToggle, CreateLink, InsertFrontmatter, InsertImage, InsertTable, InsertThematicBreak, ListsToggle, MDXEditor, UndoRedo, codeBlockPlugin, frontmatterPlugin, headingsPlugin, imagePlugin, linkDialogPlugin, linkPlugin, listsPlugin, markdownShortcutPlugin, quotePlugin, tablePlugin, thematicBreakPlugin, toolbarPlugin } from '@mdxeditor/editor'
+import '@mdxeditor/editor/style.css'
 
+// TODO - Add autosave using useffect and timer
+// TODO - modify the someSTyle class for ligth mode and more
+// TODO - Check here for more toolbar stuff: https://mdxeditor.dev/editor/docs/customizing-toolbar
+// TODO - Handle responsiveness
 export function ViewMarkdown({note}:{note: Types.NoteType}){
     return (
-        <ReactMarkdown
-            components={{
-                img: ({src="", alt=""}) =>{
-                    const [label, width] = alt.split("|");
-                    const style = width ? { width:`${width}px` } : undefined;
-                    return (
-                        <img src={src} alt={label} className={`my-4`} style={style} />
-                    )
-                }    
-            }}
-        >
-            {note?.content}
-        </ReactMarkdown>
+        <div >
+            <MDXEditor markdown={note?.content ?? "# empty"} plugins={
+                    [headingsPlugin(),
+                    listsPlugin(),
+                    markdownShortcutPlugin(),
+                    quotePlugin(),
+                    thematicBreakPlugin(),
+                    linkPlugin(),
+                    linkDialogPlugin(),
+                    codeBlockPlugin(),
+                    imagePlugin(),
+                    tablePlugin(),
+                    toolbarPlugin({
+                        toolbarClassName: 'toolbarStyle',
+                        toolbarContents: () => (
+                            <>
+                                <UndoRedo />
+                                <BoldItalicUnderlineToggles />
+                                <BlockTypeSelect/>
+                                <CodeToggle/>
+                                <CreateLink/>
+                                <InsertImage/>
+                                <InsertTable/>
+                                <InsertThematicBreak/>
+                                <ListsToggle/>
+                            </>
+                        )
+                    })
+                    ]
+                }
+                contentEditableClassName="someSTyle" // Change editor styling
+            />
+        </div>
     )
 }
